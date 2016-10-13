c7f775f28e8461a86c1bcd8aa499ad6aa42df2be
St. John, Brady <Brady.St.John@cdk.com>
2016-10-13 11:22:56 -0700

    Adding a slider at the bottom

--- js/main.js
+++ js/main.js
@@ -3,23 +3,58 @@
 var canvas = document.getElementById('myCanvas');
 var ctx = canvas.getContext('2d');
 
-// Coordinates
-var ballX = canvas.width / 2;
-var ballY = canvas.height - 30;
-var dx = 2;
-var dy = -2;
-var paddleZ = (canvas.width - paddleWidth) / 2;
+// Listen for keyboard events
+document.addEventListener('keydown', keyDownHandler, false);
+document.addEventListener('keyup', keyUpHandler, false);
+
+// Constants
+var RIGHT_CURSOR_CODE = 39;
+var LEFT_CURSOR_CODE = 37;
 
 // Properties
 var ballRadius = 10;
 var paddleHeight = 10;
 var paddleWidth = 75;
 
+// Coordinates
+var ballX = canvas.width / 2;
+var ballY = canvas.height - 30;
+var dx = 2;
+var dy = -2;
+var paddleX = (canvas.width - paddleWidth) / 2;
+
+// Controls
+var rightPressed = false;
+var leftPressed = false;
+
+function keyDownHandler(e) {
+    if (e.keyCode === RIGHT_CURSOR_CODE) {
+        rightPressed = true;
+        return;
+    }
+
+    if (e.keyCode === LEFT_CURSOR_CODE) {
+        leftPressed = true;
+    }
+}
+
+function keyUpHandler(e) {
+    if (e.keyCode === RIGHT_CURSOR_CODE) {
+        rightPressed = false;
+        return;
+    }
+
+    if (e.keyCode === LEFT_CURSOR_CODE) {
+        leftPressed = false;
+    }
+}
+
 function updateTable() {
     document.getElementById('x-row').innerHTML = ballX;
     document.getElementById('y-row').innerHTML = ballY;
     document.getElementById('dx-row').innerHTML = dx;
     document.getElementById('dy-row').innerHTML = dy;
+    document.getElementById('paddle-x-row').innerHTML = paddleX;
 }
 
 function drawBall() {
@@ -30,12 +65,18 @@ function drawBall() {
     ctx.closePath();
 }
 
+function drawPaddle() {
+    ctx.beginPath();
+    ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight);
+    ctx.fillStyle = '#0095DD';
+    ctx.fill();
+    ctx.closePath();
+}
+
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     drawBall();
-    updateTable();
-    ballX += dx;
-    ballY += dy;
+    drawPaddle();
 
     if (ballY + dy < ballRadius || ballY + dy > canvas.height - ballRadius) {
         dy = -dy;
@@ -44,6 +85,19 @@ function draw() {
     if (ballX + dx < ballRadius || ballX + dx > canvas.width - ballRadius) {
         dx = - dx;
     }
+
+    if (rightPressed && paddleX < canvas.width-paddleWidth) {
+        paddleX += 7;
+    } else if (leftPressed && paddleX > 0) {
+        paddleX -= 7;
+    }
+
+    // console.log(paddleX);
+
+    ballX += dx;
+    ballY += dy;
+
+    updateTable();
 }
 
 // Execute the draw method every 10 milliseconds
